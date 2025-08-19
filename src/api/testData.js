f706964221146e17f6e9c2ddab6a39a3fae724d9
Mysterio1001 <holmes9073@gmail.com>
2025-08-19 09:38:38 +0800

    feat: add fake api for test

---
+++ src/api/testData.js
@@ -0,0 +1,79 @@
+// new
+
+export const cardsDataTest = [
+  {
+    imgSrc: "../../public/img/image-news/list8.png",
+    title: "新到貨！巨型鍬形蟲限量登場",
+    content:
+      "來自東南亞的巨型鍬形蟲正式開賣，數量有限，想收藏的朋友手腳要快！來自東南亞的巨型鍬形蟲正式開賣，數量有限，想收藏的朋友手腳要快！來自東南亞的巨型鍬形蟲正式開賣，數量有限，想收藏的朋友手腳要快",
+    type: "announcement",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list9.png",
+    title: "活動開跑：蟲蟲大賽報名中",
+    content:
+      "第一屆蟲蟲格鬥大會即將舉行，報名即可獲得參賽禮，還有機會贏得稀有蟲種！",
+    type: "event",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list1.png",
+    title: "五角大兜蟲成功繁殖 100 隻！",
+    content: "本季首批繁殖成功，幼蟲健康活潑，即日起開放預購。",
+    type: "breedingInfo",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list2.png",
+    title: "系統維護通知",
+    content: "網站將於週五凌晨 1 點至 3 點進行維護，期間暫停服務，敬請見諒。",
+    type: "announcement",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list3.png",
+    title: "暑假親子飼育教室開放報名",
+    content: "專為親子設計的飼育體驗課程，從基礎到進階一次學會，歡迎報名參加！",
+    type: "event",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list4.png",
+    title: "黃金鹿角鍬形蟲孵化進度曝光",
+    content: "第一輪卵已成功孵化 32 隻幼蟲，預計 2 週後可分箱。",
+    type: "breedingInfo",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list5.png",
+    title: "網站改版上線囉！",
+    content: "全新 UI 與更佳的購物體驗，快來看看有哪些新功能！",
+    type: "announcement",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list6.png",
+    title: "飼育小教室：如何分辨蟲蟲性別？",
+    content: "從外觀、角度、顏色教你快速判斷公母，養蟲初學者必看！",
+    type: "breedingInfo",
+  },
+  {
+    imgSrc: "../../public/img/image-news/list7.png",
+    title: "秋季蟲展即將開幕",
+    content:
+      "全台最大規模昆蟲展覽，超過 100 種珍稀蟲種，現場還有飼育比賽與抽獎！",
+    type: "event",
+  },
+];
+
+export const swiperDataTest = [
+  {
+    src: "../../public/img/image-news/slide1.png",
+    name: "slide1.png",
+    message: "五角大兜",
+  },
+  {
+    src: "../../public/img/image-news/list2.png",
+    name: "list2.png",
+    message: "日曆",
+  },
+  {
+    src: "../../public/img/image-news/slide3.png",
+    name: "slide3.png",
+    message: "黃帶天牛",
+  },
+];
