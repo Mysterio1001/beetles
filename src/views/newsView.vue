7778c36420ae6e400bef72de41476a3725b1a2a8
Mysterio1001 <holmes9073@gmail.com>
2025-08-19 09:38:15 +0800

    feat: news update (add component: dialog, style), update i18n

--- src/views/newsView.vue
+++ src/views/newsView.vue
@@ -5,15 +5,19 @@
     <!-- 過濾點選tags -->
     <be-tags
       :data="tagsData"
-      :option="tagsOption" />
+      :option="tagsOption"
+      @tags-click="newsFilter"
+      @ready="newsFilter" />
     <!-- 消息內容卡片   -->
     <div class="cardWrapper">
       <be-card
-        v-for="(card, index) in cardsData"
+        v-for="(card, index) in filterData"
         :key="index"
         :imgSrc="card.imgSrc"
+        :imgAlt="card.title"
         :imgPosition="isMobile ? 'left' : 'top'"
-        :clickable="true">
+        :clickable="true"
+        @click="handleClick(card, index)">
         <div class="cardContent">
           <div class="titleBox">
             <h5>
@@ -25,17 +29,39 @@
               {{ card.title }}
             </h5>
           </div>
-          <p>{{ card.content }}</p>
+          <p class="contentBox">{{ card.content }}</p>
         </div>
       </be-card>
     </div>
   </be-container>
+  <!-- 消息彈窗 -->
+  <be-dialog
+    v-model:visible="showDialog"
+    :title="currentTitle"
+    :showFooterBtn="false">
+    <div class="currentCard">
+      <div class="imgBox">
+        <img
+          :src="currentCard.imgSrc"
+          :alt="currentCard.title" />
+      </div>
+      <div class="currentContent">
+        <h5>{{ currentCard.content }}</h5>
+      </div>
+    </div>
+    <template #footer>
+      <div class="footer">
+        <a href="">{{ `➤ ${t("news.go")}` }}</a>
+      </div>
+    </template>
+  </be-dialog>
 </template>
 
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from "vue";
 import { useI18n } from "vue-i18n";
 import { Bug, Megaphone, CalendarDays, NotebookPen } from "lucide-vue-next";
+import { cardsDataTest, swiperDataTest } from "@/api/testData";
 
 const { t } = useI18n();
 
@@ -67,43 +93,19 @@ const tagsData = [
 // Refs / Reactive State 定義
 
 // const swiperData = ref([]) api傳入
-const swiperData = ref([
-  {
-    src: "../../public/img/image-news/slide1.png",
-    name: "slide1.png",
-    message: "五角大兜",
-  },
-  {
-    src: "../../public/img/image-news/list2.png",
-    name: "list2.png",
-    message: "日曆",
-  },
-  {
-    src: "../../public/img/image-news/slide3.png",
-    name: "slide3.png",
-    message: "黃帶天牛",
-  },
-]);
+const swiperData = ref(swiperDataTest);
 // const cardsData = ref([]) api傳入
-const cardsData = ref([
-  {
-    imgSrc: "../../public/img/image-news/list1.png",
-    title: "五角大兜蟲大大大量發生中~~!!",
-    content:
-      "第一輪幼蟲就破百，究竟還會有多少幼蟲呢?現在特價販售中，趕快去商店瞧瞧!",
-    type: "breedingInfo",
-  },
-  {
-    imgSrc: "../../public/img/image-news/list1.png",
-    title: "五角大兜蟲",
-    content:
-      "第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，第一輪幼蟲就破百，!",
-    type: "announcement",
-  },
-]);
-
+// 傳入的照片需要統一大小
+const cardsData = ref(cardsDataTest);
+const filterData = ref([]);
+// tags 點選後的值
+const currentTag = ref("");
+// 彈窗相關
+const showDialog = ref(false);
+const currentTitle = ref("");
+const currentCard = ref({});
 // RWD 監控
-const isMobile = ref(window.innerWidth <= 768);
+const isMobile = ref(false);
 
 // Computed 計算屬性
 
@@ -122,11 +124,32 @@ const currentIcon = (type) => {
       return Bug;
   }
 };
+// 消息過濾
+const newsFilter = (val) => {
+  // 如果是已經點選的不執行
+  if (val === currentTag.value) return;
+  // 儲存這次點選的值
+  currentTag.value = val;
+  if (val === "all") {
+    filterData.value = cardsData.value;
+  } else {
+    filterData.value = cardsData.value.filter((item) => item.type === val);
+  }
+};
+
+// 卡片點選
+const handleClick = (card, index) => {
+  currentTitle.value = card.title;
+  showDialog.value = true;
+  currentCard.value = card;
+};
 
 // 監控視窗變化
 const updateSize = () => {
-  isMobile.value = window.innerWidth <= 768;
+  isMobile.value = window.innerWidth <= 576;
 };
+
+// api
 
 // Watchers
 
