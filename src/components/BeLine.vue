36e1f58c7c8a4fe467e895ba5c17b82b705c1f7c
Mysterio1001 <holmes9073@gmail.com>
2025-08-20 16:59:56 +0800

    feat: add component: BeLine.vue

---
+++ src/components/BeLine.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    :class="[
+      'be-line',
+      { vertical: direction === 'vertical' },
+      { lightMode },
+      { hasMargin: margin },
+    ]" />
+</template>
+
+<script setup>
+import { ref, defineProps, defineEmits } from "vue";
+
+// defineProps / defineEmits
+const props = defineProps({
+  // 垂直 horizontal | vertical
+  direction: { type: String, default: "horizontal" },
+  // 淺色模式
+  lightMode: {
+    type: Boolean,
+    default: false,
+  },
+  // 是否有margin
+  margin: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const emit = defineEmits([]);
+
+// Refs / Reactive State 定義
+
+// Computed 計算屬性
+
+// Methods / Functions
+
+// Watchers
+
+// Lifecycle Hooks
+</script>
+
+<style lang="scss" scoped>
+@use "sass:map";
+
+.be-line {
+  border: 0.8px solid getColor(black);
+  width: 100%;
+  border-radius: radius("block");
+
+  &.vertical {
+    width: 0;
+    height: 100%;
+  }
+  &.lightMode {
+    border: 0.8px solid getColor(white);
+  }
+  &.hasMargin {
+    margin: 1rem 0 1rem;
+  }
+}
+</style>
